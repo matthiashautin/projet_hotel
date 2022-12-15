d31ed3a2765dc19846f717292b1052febb1596c6
matthiashautin <matthiashautin@gmail.com>
2022-12-15 10:24:18 +0100

    client.php

---
+++ accueil.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width==device-width, initial-scale=1.0">
+    <title>CVVEN</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+    <header class="header">
+
+        <div id="menu-btn" class="fas fa-bars icons"></div>
+        <?php include("nav.php") ?>
+        <div class="form-location">
+            <center>
+                <h1>CVVEN</h1>
+                <img src="http://theopruski.alwaysdata.net/TP_Location/Images/maison-contemporaine.jpg" width="50%">
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus enim, quasi aspernatur veniam iusto molestias accusamus fugit sit ratione accusantium perspiciatis provident consequuntur porro beatae possimus velit quam, quo tenetur?</p>
+                <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Corporis, accusamus perspiciatis inventore et deleniti soluta repellat incidunt architecto sequi nulla similique, voluptates ipsum itaque ex distinctio neque enim reprehenderit eius.</p>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut quos id libero ipsa in quis dolores ipsum, rem autem quae facilis corporis cumque quisquam quia nisi a amet tempora. Maiores.</p>
+            </center>
+        </div>
+</body>
+
+</html>

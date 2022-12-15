edd378d2bdcd7d3cff47716e51c9bc3a90821fd9
theopruski <theo.pruski@gmail.com>
2022-12-15 09:15:26 +0100

    modif conndb.php

--- AlwaysData-CVVEN-AUTH/CVVEN/conndb.php
+++ AlwaysData-CVVEN-AUTH/CVVEN/conndb.php
@@ -3,14 +3,11 @@ $host = 'mysql-cvven.alwaysdata.net';
 $dbname = 'cvven_bdd_projet_hotel';
 $username = 'cvven';
 $password = 'G;d,Q7)=4wXj36qL';
-$dsn = '';
+$dsn = "mysql:host=$host;dbname=$dbname";
 
 try {
-    $dsn = 'mysql:host' . $host . ';dbname' . $dbname;
     $pdo = new PDO($dsn, $username, $password);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $e) {
     echo 'connection failed: ' . $e->getMessage();
 }
-
-

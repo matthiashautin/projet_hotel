449e4bc5fa3bfd8af123caa3396c0676772e71dc
theopruski <theo.pruski@gmail.com>
2023-01-05 11:41:45 +0100

    affichage user correct

--- cvven/Controleur/clogin.php
+++ cvven/Controleur/clogin.php
@@ -1,46 +1,44 @@
 <?php
-include_once('../Controleur/conn_db.php');  
+include_once('../Controleur/conn_db.php');
 
 session_start();
 
 if (isset($_POST['Connexion'])) {
-        $database = new Connection();
-        $db = $database->open();
-        
-        $pdoStat = $db->prepare('SELECT * FROM Client WHERE mail=:mail AND password=:password');
-       
-        $mail = $_POST['mail'];
-        $password = $_POST['password'];
-        $password=md5($password);
+    $database = new Connection();
+    $db = $database->open();
 
-        $pdoStat->bindValue(':mail', $mail, PDO::PARAM_STR_CHAR);
-        $pdoStat->bindValue(':password', $password, PDO::PARAM_STR_CHAR);
-        $pdoStat->execute();
+    $pdoStat = $db->prepare('SELECT * FROM Client WHERE mail=:mail AND password=:password');
 
-        $count = $pdoStat->rowCount();
-        $rows = $pdoStat->fetch(PDO::FETCH_ASSOC);
+    $mail = $_POST['mail'];
+    $password = $_POST['password'];
+    $password = md5($password);
 
-        if($count > 0){
-            if($rows['users_type'] == 'admin'){
+    $pdoStat->bindValue(':mail', $mail, PDO::PARAM_STR_CHAR);
+    $pdoStat->bindValue(':password', $password, PDO::PARAM_STR_CHAR);
+    $pdoStat->execute();
 
-                session_start();
-                $_SESSION['admin_id'] = $rows['ID'];
-                $message = 'Connexion réussi admin';
-                header('location:../Vue/admin_res.php');
-    
-            } else if($rows['users_type'] == 'user'){
+    $count = $pdoStat->rowCount();
+    $rows = $pdoStat->fetch(PDO::FETCH_ASSOC);
 
-                session_start();
-                $_SESSION['user_id'] = $rows['ID'];
-                $message = 'Connexion réussi user';
-                header('location:../Vue/home.php');
+    if ($count > 0) {
+        if ($rows['users_type'] == 'admin') {
 
+            session_start();
+            $_SESSION['admin_id'] = $rows['ID'];
+            $message = 'Connexion réussi admin';
+            header('location:../Vue/admin_res.php');
+        } else if ($rows['users_type'] == 'user') {
+
+            session_start();
+            $_SESSION['user_id'] = $rows['ID'];
+            $_SESSION['Nom'] = $rows['Nom'];
+            $_SESSION['Prenom'] = $rows['Prenom'];
+            $message = 'Connexion réussi user';
+            header('location:../Vue/home.php');
         } else {
             $message = "Impossible de se connecter vérifier vos identifiant. ❌";
+        }
+    } else {
+        $message = 'incorrect email or password!';
     }
-} else{
-    $message = 'incorrect email or password!';
 }
-}
-
-
--- cvven/Vue/header.php
+++ cvven/Vue/header.php
@@ -2,18 +2,22 @@
 require_once '../Controleur/conn_db.php';
 session_start();
 
-$nom = isset($_SESSION['Nom']) ? $_SESSION['Nom'] : '';
-if (isset($_SESSION['loggedin']) && $_SESSION['loggedin'] == true) {
+$database = new Connection();
+$db = $database->open();
+
+$nom = isset($_SESSION['Nom']) ? $_SESSION['Nom'] : 'Erreur';
+$prenom = isset($_SESSION['Nom']) ? $_SESSION['Nom'] : 'Erreur';
+/*if (isset($_SESSION['loggedin']) && $_SESSION['loggedin'] == true) {
     echo "Welcome to the member's area, " . $_SESSION['Nom'] . "!";
 } else {
     echo "Please log in first to see this page.";
 }
-
+*/
 ?>
 
 <header>
     <img src="../Image/hotel.png" alt="logo">
     <h4 class="name-user">
-        <p>Bonjour,</p><?php echo $nom ?>
+        <p>Bonjour, <?php echo $nom . " " . $prenom ?></p>
     </h4>
 </header>

4142cde3a08f4957b631edc5bc49ea426a5b4889
theopruski <theo.pruski@gmail.com>
2023-01-05 10:09:28 +0100

    Update header.php

--- cvven/Vue/header.php
+++ cvven/Vue/header.php
@@ -1,9 +1,8 @@
-<?php/*
+<?php
 require_once '../Controleur/conn_db.php';
 session_start();
 
-$nom = isset($_SESSION['Nom']) ? $_SESSION['Nom']: '';*/
-session_start();
+$nom = isset($_SESSION['Nom']) ? $_SESSION['Nom'] : '';
 if (isset($_SESSION['loggedin']) && $_SESSION['loggedin'] == true) {
     echo "Welcome to the member's area, " . $_SESSION['Nom'] . "!";
 } else {
@@ -13,6 +12,8 @@ if (isset($_SESSION['loggedin']) && $_SESSION['loggedin'] == true) {
 ?>
 
 <header>
-   <img src="../Image/hotel.png" alt="logo">
-   <h4 class="name-user"><p>Bonjour,</p><?php echo $nom?> </h4> 
+    <img src="../Image/hotel.png" alt="logo">
+    <h4 class="name-user">
+        <p>Bonjour,</p><?php echo $nom ?>
+    </h4>
 </header>

77b4d1a6338f884c6580a26ea5ae6e749abdbc66
theopruski <theo.pruski@gmail.com>
2023-01-17 12:17:59 +0100

    modif reservation

--- cvven/Modele/admin_res/add.php
+++ cvven/Modele/admin_res/add.php
@@ -10,7 +10,7 @@ if (isset($_POST['add'])) {
         //preparer la sql injection pour la table animation
         $stmt = $db->prepare("INSERT INTO Reservation (Hebergement_ID, Client_ID, Restauration_ID, Animation_ID, Region_ID, DateDebut, DateFin) VALUES (:Hebergement_ID, :Client_ID, :Restauration_ID, :Animation_ID, :Region_ID, :DateDebut, :DateFin)");
         //excecuter l'injection sql instruction if-else dans l'exécution de notre requête
-        $_SESSION['message'] = ($stmt->execute(array(':Hebergement_ID' => $_POST['Hebergement_ID'], ':Client_ID' => $_POST['Client_ID'], ':Restauration_ID' => $_POST['Restauration_ID'], ':Animation_ID' => $_POST['Animation_ID'], ':Region_ID' => $_POST['Region_ID'], ':DateDebut' => $_POST['DateDebut'], ':DateFin' => $_POST['DateFin'],))) ? "Nouvelle Réservation ajouté avec succès" : "Une erreur s'est produite. Impossible d'ajouter cette Réservation";
+        $_SESSION['message'] = ($stmt->execute(array(':Hebergement_ID' => $_POST['Hebergement_ID'], ':Client_ID' => $_POST['Client_ID'], ':Restauration_ID' => $_POST['Restauration_ID'], ':Animation_ID' => $_POST['Animation_ID'], ':Region_ID' => $_POST['Restauration_ID'], ':DateDebut' => $_POST['DateDebut'], ':DateFin' => $_POST['DateFin'],))) ? "Nouvelle Réservation ajouté avec succès" : "Une erreur s'est produite. Impossible d'ajouter cette Réservation";
     } catch (PDOException $e) {
         $_SESSION['message'] = $e->getMessage();
     }

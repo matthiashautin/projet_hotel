9d6e75778700b29a5778ac506a75a1800dbe2c85
theopruski <theo.pruski@gmail.com>
2023-01-17 09:55:19 +0100

    reservation

---
+++ cvven/Modele/admin_res/edit.php
@@ -0,0 +1,33 @@
+<?php
+session_start();
+include_once('../../Controleur/conn_db.php');
+
+if (isset($_POST['edit'])) {
+    //connection
+    $database = new Connection();
+    $db = $database->open();
+    try {
+        //création des variables 
+        $id = $_GET['ID'];
+        $Hebergement_ID = $_POST['Hebergement_ID'];
+        $Client_ID = $_POST['Client_ID'];
+        $Restauration_ID = $_POST['Restauration_ID'];
+        $Animation_ID = $_POST['Animation_ID'];
+        $Region_ID = $_POST['Region_ID'];
+        $DateDebut = $_POST['DateDebut'];
+        $DateFin = $_POST['DateFin'];
+
+        //preparer la sql injection pour la table animation
+        $sql = "UPDATE Reservation SET Hebergement_ID = '$Hebergement_ID', Client_ID  = '$Client_ID', Restauration_ID  = '$Restauration_ID', Animation_ID = '$Animation_ID', Region_ID = '$Region_ID ', DateDebut = '$DateDebut' , DateFin = '$DateFin' WHERE ID = '$id'";
+        //excecuter l'injection sql instruction if-else dans l'exécution de notre requête
+        $_SESSION['message'] = ($db->exec($sql)) ? 'Reservation mis à jour avec succès' : 'Une erreur est survenue. Impossible de mettre à jour cette réservation';
+    } catch (PDOException $e) {
+        $_SESSION['message'] = $e->getMessage();
+    }
+    //close connection
+    $database->close();
+} else {
+    $_SESSION['message'] = 'Remplissez en premier le formulaire de modification';
+}
+
+header('location: ../../Vue/admin_res.php');
---
+++ cvven/Modele/admin_res/reservation.php
@@ -0,0 +1,32 @@
+<?php
+include_once('../Controleur/conn_db.php');
+
+$database = new Connection();
+$db = $database->open();
+try {
+    $sql = 'SELECT * FROM Reservation';
+    foreach ($db->query($sql) as $row) {
+?>
+        <tr>
+            <td><?php echo htmlspecialchars($row['ID']); ?></td>
+            <td><?php echo htmlspecialchars($row['Hebergement_ID']); ?></td>
+            <td><?php echo htmlspecialchars($row['Client_ID']); ?></td>
+            <td><?php echo htmlspecialchars($row['Restauration_ID']); ?></td>
+            <td><?php echo htmlspecialchars($row['Animation_ID']); ?></td>
+            <td><?php echo htmlspecialchars($row['Region_ID']); ?></td>
+            <td><?php echo htmlspecialchars($row['DateDebut']); ?></td>
+            <td><?php echo htmlspecialchars($row['DateFin']); ?></td>
+            <td class="td-edit-delete">
+                <a href="#edit_<?php echo htmlspecialchars($row['ID']); ?>" class="btn-edit" data-bs-toggle="modal"> Modifier</a>
+                <a href="#delete_<?php echo htmlspecialchars($row['ID']); ?>" class="btn-delete" data-bs-toggle="modal"> Supprimer</a>
+            </td>
+            <?php include('../Vue/reservation/edit_delete_reservation.php'); ?>
+        </tr>
+<?php
+    }
+} catch (PDOException $e) {
+    echo 'Il y a un problème de connexion :' . $e->getMessage();
+}
+//close connection
+$database->close();
+?>

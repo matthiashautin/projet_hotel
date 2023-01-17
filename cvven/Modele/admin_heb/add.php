c41188f48455b3bd88571ed66526201c8f8fe0c5
theopruski <theo.pruski@gmail.com>
2023-01-17 12:05:35 +0100

    modif hebergement

--- cvven/Modele/admin_heb/add.php
+++ cvven/Modele/admin_heb/add.php
@@ -1,24 +1,4 @@
 <?php
-
-    session_start();
-    include_once('../../Controleur/conn_db.php');
-    
-    if(isset($_POST['add'])){
-        //connection
-        $database = new Connection();
-        $db = $database->open();
-        try{
-            //preparer la sql injection pour la table animation
-            $stmt = $db->prepare("INSERT INTO Restauration (Chambre_Doubles, Chambres_3_Lits, Chambres_4_Lits, Logement_Handi, Menage) VALUES (:Chambre_Doubles, :Chambres_3_Lits, :Chambres_4_Lits, :Logement_Handi, :Menage)");
-            //excecuter l'injection sql instruction if-else dans l'exécution de notre requête
-            $_SESSION['message'] = ( $stmt->execute(array(':Chambre_Doubles' => $_POST['Chambre_Doubles'] , ':Chambres_3_Lits' => $_POST['Chambres_3_Lits'] , ':Chambres_4_Lits' => $_POST['Chambres_4_Lits'] , ':Logement_Handi' => $_POST['Logement_Handi'], ':Menage' => $_POST['Menage'])) ) ? "Hebergement ajouté avec succès" : "Une erreur s'est produite. Impossible d'ajouter cette hebergement";
-        }
-        catch(PDOException $e){
-            $_SESSION['message'] = $e->getMessage();
-        }
-        //close connection
-        $database->close();
-    }
 session_start();
 include_once('../../Controleur/conn_db.php');
 
@@ -28,16 +8,12 @@ if (isset($_POST['add'])) {
     $db = $database->open();
     try {
         //preparer la sql injection pour la table animation
-        $stmt = $db->prepare("INSERT INTO Hebergement (Chambres_doubles, Chambres_3_Lits, Chambres_4_Lits, Logement_Handi, Menage) VALUES (:Chambre_Doubles, :Chambres_3_Lits, :Chambres_4_Lits, :Logement_Handi, :Menage)");
+        $stmt = $db->prepare("INSERT INTO Restauration (Chambre_Doubles, Chambres_3_Lits, Chambres_4_Lits, Logement_Handi, Menage) VALUES (:Chambre_Doubles, :Chambres_3_Lits, :Chambres_4_Lits, :Logement_Handi, :Menage)");
         //excecuter l'injection sql instruction if-else dans l'exécution de notre requête
-        $_SESSION['message'] = ($stmt->execute(array(':Chambres_doubles' => $_POST['Chambres_doubles'], ':Chambres_3_Lits' => $_POST['Chambres_3_Lits'], ':Chambres_4_Lits' => $_POST['Chambres_4_Lits'], ':Logement_Handi' => $_POST['Logement_Handi'], ':Menage' => $_POST['Menage']))) ? "Hebergement ajouté avec succès" : "Une erreur s'est produite. Impossible d'ajouter cette hebergement";
+        $_SESSION['message'] = ($stmt->execute(array(':Chambre_Doubles' => $_POST['Chambre_Doubles'], ':Chambres_3_Lits' => $_POST['Chambres_3_Lits'], ':Chambres_4_Lits' => $_POST['Chambres_4_Lits'], ':Logement_Handi' => $_POST['Logement_Handi'], ':Menage' => $_POST['Menage']))) ? "Hebergement ajouté avec succès" : "Une erreur s'est produite. Impossible d'ajouter cette hebergement";
     } catch (PDOException $e) {
         $_SESSION['message'] = $e->getMessage();
-
     }
     //close connection
     $database->close();
-} else {
-    $_SESSION['message'] = "Remplissez d'abord le formulaire d'ajout";
 }
-header('location: ../../Vue/admin_heb.php');

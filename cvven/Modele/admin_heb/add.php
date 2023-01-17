02b1b0d652da2ca867640da38a732f9fb02e7240
theopruski <theo.pruski@gmail.com>
2023-01-17 11:57:58 +0100

    modif hebergement

--- cvven/Modele/admin_heb/add.php
+++ cvven/Modele/admin_heb/add.php
@@ -1,24 +1,22 @@
 <?php
-    session_start();
-    include_once('../../Controleur/conn_db.php');
-    
-    if(isset($_POST['add'])){
-        //connection
-        $database = new Connection();
-        $db = $database->open();
-        try{
-            //preparer la sql injection pour la table animation
-            $stmt = $db->prepare("INSERT INTO Hebergement (Chambres_doubles, Chambres_3_Lits, Chambres_4_Lits, Logement_Handi, Ménage) VALUES (:Chambre_Doubles, :Chambres_3_Lits, :Chambres_4_Lits, :Logement_Handi, :Ménage)");
-            //excecuter l'injection sql instruction if-else dans l'exécution de notre requête
-            $_SESSION['message'] = ( $stmt->execute(array(':Chambres_doubles' => $_POST['Chambres_doubles'] , ':Chambres_3_Lits' => $_POST['Chambres_3_Lits'] , ':Chambres_4_Lits' => $_POST['Chambres_4_Lits'] , ':Logement_Handi' => $_POST['Logement_Handi'], ':Ménage' => $_POST['Ménage'])) ) ? "Hebergement ajouté avec succès" : "Une erreur s'est produite. Impossible d'ajouter cette hebergement";
-        }
-        catch(PDOException $e){
-            $_SESSION['message'] = $e->getMessage();
-        }
-        //close connection
-        $database->close();
+session_start();
+include_once('../../Controleur/conn_db.php');
+
+if (isset($_POST['add'])) {
+    //connection
+    $database = new Connection();
+    $db = $database->open();
+    try {
+        //preparer la sql injection pour la table animation
+        $stmt = $db->prepare("INSERT INTO Hebergement (Chambres_doubles, Chambres_3_Lits, Chambres_4_Lits, Logement_Handi, Menage) VALUES (:Chambre_Doubles, :Chambres_3_Lits, :Chambres_4_Lits, :Logement_Handi, :Menage)");
+        //excecuter l'injection sql instruction if-else dans l'exécution de notre requête
+        $_SESSION['message'] = ($stmt->execute(array(':Chambres_doubles' => $_POST['Chambres_doubles'], ':Chambres_3_Lits' => $_POST['Chambres_3_Lits'], ':Chambres_4_Lits' => $_POST['Chambres_4_Lits'], ':Logement_Handi' => $_POST['Logement_Handi'], ':Ménage' => $_POST['Ménage']))) ? "Hebergement ajouté avec succès" : "Une erreur s'est produite. Impossible d'ajouter cette hebergement";
+    } catch (PDOException $e) {
+        $_SESSION['message'] = $e->getMessage();
     }
-    else{
+    //close connection
+    $database->close();
+} else {
     $_SESSION['message'] = "Remplissez d'abord le formulaire d'ajout";
-    }
-    header('location: ../../Vue/admin_heb.php');
+}
+header('location: ../../Vue/admin_heb.php');

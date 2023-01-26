3f86e0ff4ec766ba931c8c298f664df09d0fbfea
Joris.R <joris.rkto@gmail.com>
2023-01-26 08:41:51 +0100

    test

--- cvven/Vue/admin_res.php
+++ cvven/Vue/admin_res.php
@@ -52,5 +52,5 @@ include "./common/menu.php";
     <script src="https://cdn.jsdelivr.net/npm/jquery@3.6.0/dist/jquery.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js"></script>
 </body>
-
+<!-- sjocnococidhncodcndo -->
 </html>

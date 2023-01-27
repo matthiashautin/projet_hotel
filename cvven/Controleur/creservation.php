cfbb9d9010b27a5693492689a5716c39feef618f
Joris.R <joris.rkto@gmail.com>
2023-01-27 17:34:00 +0100

    image + formualaire pour chaque hotel

--- cvven/Controleur/creservation.php
+++ cvven/Controleur/creservation.php
@@ -2,55 +2,38 @@
 include "../Controleur/connectuser.php";
 
 if ((isset($_GET['region']) && $_GET['region'] == "LaRochelle")) { ?>
-        <div class="add_reservation_user">
+    <div class="add_reservation_user">
         <?php include('../Vue/reservation_user/add_reservation_rochelle.php'); ?>
-        </div>
+    </div>
+
 <?php
 } else if ((isset($_GET['region']) && $_GET['region'] == "LesRousses")) { ?>
-    <main class="title-region">
-        <h1>Hotel Les Rousses</h1>
-    </main>
-    <main class="title-p">
-        <p>(Jura)</p>
-    </main>
-    <section class="add_reservation_user">
+    <div class="add_reservation_user">
         <?php include('../Vue/reservation_user/add_reservation_rousses.php'); ?>
-    </section>
+    </div>
 
 <?php
 } else if ((isset($_GET['region']) && $_GET['region'] == "SaintAnthème")) { ?>
-    <main class="title-region">
-        <h1>Hotel Saint-Anthème</h1>
-    </main>
-    <main class="title-p">
-        <p>(Puy-de-Dôme)</p>
-    </main>
-    <section class="add_reservation_user">
+    <div class="add_reservation_user">
         <?php include('../Vue/reservation_user/add_reservation_antheme.php'); ?>
-    </section>
-<?php
+    </div>
 
+<?php
 } else if ((isset($_GET['region']) && $_GET['region'] == "Villefort")) { ?>
-    <main class="title-region">
-        <h1>Hotel Villefort</h1>
-    </main>
-    <main class="title-p">
-        <p>(Lozère)</p>
-    </main>
-    <section class="add_reservation_user">
+    <div class="add_reservation_user">
         <?php include('../Vue/reservation_user/add_reservation_villefort.php'); ?>
-    </section>
-<?php
+    </div>
 
+<?php
 } else if ((isset($_GET['region']) && $_GET['region'] == "reservation")) { ?>
     <main class="title-region">
         <h1>Vos réservation</h1>
     </main>
     <div class="container">
         <div class="rows">
-                <?php
-                    include('../Controleur/messageadmin.php');
-                ?>
+            <?php
+            include('../Controleur/messageadmin.php');
+            ?>
             <table class="table table-bordered table-striped">
                 <thead>
                     <th class="top-th">Logements</th>
@@ -62,13 +45,13 @@ if ((isset($_GET['region']) && $_GET['region'] == "LaRochelle")) { ?>
                     <th class="top-th">datefin</th>
                     <th class="top-th">Action</th>
                 </thead>
-                    <?php
-                    include '../Modele/user_res/vos_reservation.php';
-                    ?>
+                <?php
+                include '../Modele/user_res/vos_reservation.php';
+                ?>
             </table>
         </div>
     </div>
-<?php 
+<?php
 
 } else {
     header('location:./home.php');
--- cvven/Vue/reservation_user/add_reservation_villefort.php
+++ cvven/Vue/reservation_user/add_reservation_villefort.php
@@ -10,29 +10,34 @@ $restauration = "SELECT Type_Resto FROM Restauration";
 $region = "SELECT Nom_Region FROM Region  WHERE ID='7'";
 
 ?>
-<article class="card">
-<header class="card__thumb">
-  <a href="#">
-    <img src="http://lorempicsum.com/futurama/370/235/2">
-  </a>
-</header>
-<div class="card__date">
-  <span class="card__date__day">12</span>
-  <span class="card__date__month">Mai</span>
-</div>
-<div class="card__body">
-  <div class="card__category"><a href="#">Photos</a></div>
-  <h2 class="card__title"><a href="#">Bender Should Not Be Allowed on TV</a></h2>
-  <div class="card__subtitle">A Head in the Polls</div>
-  <p class="card__description">
-    With a warning label this big, you know they gotta be fun! This is the worst part. The calm before the battle. No! The cat shelter's on to me. Yes, I saw. You were doing well, until everyone died. Daylight and everything.
-  </p>
-</div>
-<footer class="card__footer">
-  <span class="icon icon--time"></span>6 min
-  <span class="icon icon--comment"></span><a href="#">39 comments</a>
-</footer>
-</article>
+<main class="title-region">
+    <h1>Hotel Villefort</h1>
+</main>
+<main class="title-p">
+    <p>(Lozère)</p>
+</main>
+<section class="king_card">
+    <div class="card">
+        <img src="../Image/chambre-parentale-couleurs-et-deco-img_f2919f850e836ba8_14-2403-1-78ef3ac.jpg" alt="Avatar" style="width:100%">
+        <div class="image-galerie">
+            <div class="test">
+                <h4><b>Chambre doubles Beluga</b></h4>
+                <p>Voici une de nos chambres d'hôtel spacieuses et lumineuses, avec des équipements modernes pour assurer un séjour confortable.<br>
+                    Vous pourrez vous détendre sur un lit moelleux. Nous proposons également différentes animations qui vous conviendront certainement.</p>
+            </div>
+        </div>
+    </div>
+    <div class="card">
+        <img src="../Image/gvalc-penthouse-suite-6918-hor-clsc.jpg" alt="Avatar" style="width:100%">
+        <div class="image-galerie">
+            <div class="test">
+                <h4><b>Chambre doubles du Lion d'Or</b></h4>
+                <p>La Chambre Lion d'Or est un véritable joyau de notre hôtel. <br>
+                    Conçue pour offrir un séjour de luxe et de confort, elle est décorée dans des tons chauds et élégants qui rappellent la noblesse.
+                    La chambre est spacieuse et lumineuse, elle est équipée d'un lit king-size confortable avec une literie haut de gamme.</p>
+            </div>
+        </div>
+    </div>
     <div class="modal-dialog">
         <div class="modal-content">
             <div class="modal-header">
@@ -40,55 +45,55 @@ $region = "SELECT Nom_Region FROM Region  WHERE ID='7'";
             </div>
             <div class="modal-body">
                 <form method="POST" action="../Modele/user_res/add.php">
-                <div class="mb-3 row">
+                    <div class="mb-3 row">
                         <label class="col-sm-2 col-form-label">Logement</label>
                         <div class="col-sm-10">
-                        <select name="Logements">
-                        <option  value="">Selectionnez un Logement</option>
-                            <?php 
-                            foreach ($db->query($logements) as $row) {
-                            ?> 
-                                <option value="<?php echo $row['Logements'] ?>"><?php echo htmlspecialchars($row['Logements']); ?></option>
-                        <?php    } ?>
-                        </select>
+                            <select name="Logements">
+                                <option value="">Selectionnez un Logement</option>
+                                <?php
+                                foreach ($db->query($logements) as $row) {
+                                ?>
+                                    <option value="<?php echo $row['Logements'] ?>"><?php echo htmlspecialchars($row['Logements']); ?></option>
+                                <?php    } ?>
+                            </select>
                         </div>
                     </div>
                     <div class="mb-3 row">
                         <label class="col-sm-2 col-form-label">Restauration</label>
                         <div class="col-sm-10">
-                        <select name="Type_Resto">
-                        <option value="">Selectionnez une Restauration</option>
-                            <?php 
-                            foreach ($db->query($restauration) as $row) {
-                            ?>
+                            <select name="Type_Resto">
+                                <option value="">Selectionnez une Restauration</option>
+                                <?php
+                                foreach ($db->query($restauration) as $row) {
+                                ?>
                                     <option value="<?php echo $row['Type_Resto'] ?>"><?php echo htmlspecialchars($row['Type_Resto']); ?></option>
-                        <?php    } ?>
-                        </select>
+                                <?php    } ?>
+                            </select>
                         </div>
                     </div>
                     <div class="mb-3 row">
                         <label class="col-sm-2 col-form-label">Animation</label>
                         <div class="col-sm-10">
-                        <select name="Nom_Anim">
-                        <option value="">Selectionnez une Animation</option>
-                            <?php 
-                            foreach ($db->query($animation) as $row) {
-                            ?>
+                            <select name="Nom_Anim">
+                                <option value="">Selectionnez une Animation</option>
+                                <?php
+                                foreach ($db->query($animation) as $row) {
+                                ?>
                                     <option value="<?php echo $row['Nom_Anim'] ?>"><?php echo htmlspecialchars($row['Nom_Anim']); ?></option>
-                        <?php    } ?>
-                        </select>
+                                <?php    } ?>
+                            </select>
                         </div>
                     </div>
                     <div class="mb-3 row">
                         <label class="col-sm-2 col-form-label">Region</label>
                         <div class="col-sm-10">
-                        <select name="Nom_Region">
-                            <?php 
-                            foreach ($db->query($region) as $row) {
-                            ?>
+                            <select name="Nom_Region">
+                                <?php
+                                foreach ($db->query($region) as $row) {
+                                ?>
                                     <option value="<?php echo $row['Nom_Region'] ?>"><?php echo htmlspecialchars($row['Nom_Region']); ?></option>
-                        <?php    } ?>
-                        </select>
+                                <?php    } ?>
+                            </select>
                         </div>
                     </div>
                     <div class="mb-3 row">
@@ -117,4 +122,4 @@ $region = "SELECT Nom_Region FROM Region  WHERE ID='7'";
             </div>
         </div>
     </div>
-</div>
+    </div>
